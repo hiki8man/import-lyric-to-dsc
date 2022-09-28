37f21226e96ad97ea46ca7a616a1ca929c34eb37
hiki8man <63759994+hiki8man@users.noreply.github.com>
2022-09-29 00:57:14 +0800

    Update option.py

--- chart/option.py
+++ chart/option.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 
+#修改所有HOLD到最上方图层，未使用
 def change_hold_note_show(dsc_data_list):
     num = 0
     fix_dsc_data_list = dsc_data_list.copy()
@@ -25,25 +26,21 @@ def change_hold_note_show(dsc_data_list):
         list_temp.extend(list_temp2_hold)
         if len(list_temp2_hold) != 0:
             fix_dsc_data_list[num]["data"] = list_temp.copy()
-        #pprint.pprint(fix_dsc_data_list)
-        #print("------------------------------------")
         num += 1
-        #pprint.pprint(time_data)
-    #print("------------------------------------")
-    #pprint.pprint(fix_dsc_data_list)
     return fix_dsc_data_list
     
 def delete_lyric(dsc_data_list):
+    #歌词指令头数据
     op_lyric = b'\x18\x00\x00\x00'
     num = 0
     fix_dsc_data_list_temp = []
     #导入dsc
     for data in dsc_data_list:
-        #建立空白
+        #建立空白歌词
         fix_dsc_data_list_temp.append({"time":"no","data":[]})
         #检测是否有歌词
         for check_lyric in data["data"]:
-            #没有则写入
+            #没有则写入列表，有则跳过
             if check_lyric[0:4] != op_lyric:
                 fix_dsc_data_list_temp[num]["data"].append(check_lyric)
         if len(fix_dsc_data_list_temp[num]["data"]) != 0:
@@ -64,33 +61,32 @@ def merge_dsc_data(input_dsc=[],input_ass=[]):
         if len(input_ass) > i and dsc_data["time"] != None:
             time_dsc = int.from_bytes(dsc_data["time"][4:] ,byteorder='little')
             time_lyric = int.from_bytes(input_ass[i]["time"][4:] ,byteorder='little')
+            #按时间顺序添加dsc数据
+            #dsc时间比歌词时间优先时
             if time_dsc < time_lyric:
                 merge_temp2.append(dsc_data)
-                #print("a")
+            #dsc时间不比歌词时间优先时
             else:
+                #遍历对比剩下的歌词时间数据
                 for time_lyric2 in input_ass[i:]:
                     time_lyric = int.from_bytes(time_lyric2["time"][4:] ,byteorder='little')
                     if time_dsc > time_lyric:
                         merge_temp2.append(input_ass[i])
                         i += 1
-                        #print("c")
+                    #若时间完全相等则合并时间轴，并将歌词数据优先写入
                     elif time_dsc == time_lyric:
                         merge_temp = (input_ass[i])
-                        #merge_temp.append(input_ass[i])
                         merge_temp["data"] += dsc_data["data"].copy()
                         merge_temp2.append(merge_temp)
                         i += 1
-                        #print("b")
                     else:
                         merge_temp2.append(dsc_data)
-                        #print("exit")
+                       
                         break
             if i == len(input_ass):
                 merge_temp2.append(dsc_data)
-                #print("e")
         else:
             merge_temp2.append(dsc_data)
-            #print("d")
     for b in merge_temp2:
         merge_dsc_data_list.append(b)
     return merge_dsc_data_list
